cf2428783e14d3241605bfab7ae238d08714235f
bunnyhat <zee_shan265@hotmail.com>
2016-07-10 15:53:46 -0400

    revert

--- Assets/Scripts/UI Scripts/PauseManagerScript.cs
+++ Assets/Scripts/UI Scripts/PauseManagerScript.cs
@@ -36,6 +36,7 @@ public class PauseManagerScript : MonoBehaviour {
 			isPlayerOnePaused = true;
 			playerOnePauseGUI.enabled = true;
 			playerOneHud.SetActive(false);
+			playerTwoHud.SetActive(false);
 			if(incomingWave.activeInHierarchy) {
 				incomingWave.SetActive(false);
 			}
@@ -47,6 +48,7 @@ public class PauseManagerScript : MonoBehaviour {
 			isPlayerTwoPaused = true;
 			playerTwoPauseGUI.enabled = true;
 			playerTwoHud.SetActive(false);
+			playerOneHud.SetActive(false);
 			if(incomingWave.activeInHierarchy) {
 				incomingWave.SetActive(false);
 			}
